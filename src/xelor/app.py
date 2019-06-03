d627d097ff5d9bc5681e0d9e17b3b25abe0985e1
Jeremy Merle <jeremy.merle@protonmail.com>
2019-06-03 22:01:27 +0200

    Setuptools integration

--- src/xelor/app.py
+++ src/xelor/app.py
@@ -5,6 +5,8 @@ from subprocess import PIPE, Popen
 
 import click
 
+from .message import ChatMessage
+
 BIT_MASK = 3
 
 
@@ -65,42 +67,6 @@ def compute_size_category(static_header):
         return "H"
     # TODO Add other masks
     return "B"
-
-
-canals = {
-    9: "Guilde",
-    2: "Privé",
-    6: "Recrutement",
-    5: "Commerce",
-    14: "FR",
-    0: "Général",
-}
-
-
-class ChatMessage:
-    def __init__(self, data):
-        self.message = " "
-        try:
-            self.canal, _message_len = struct.unpack_from("!BH", data)
-            self.message, self.timestamp, _fingerprint, _name_len = struct.unpack_from(
-                "!{}si18sh".format(_message_len), data, offset=3
-            )
-            self.name, = struct.unpack_from(
-                "!{}s".format(_name_len), data, offset=27 + _message_len
-            )
-            self.message = codecs.decode(self.message, "utf8")
-            self.name = codecs.decode(self.name, "utf8")
-        except Exception:
-            print("Could not parse {}".format(data))
-
-    def __repr__(self):
-        if self.canal not in canals:
-            return "ERROR Canal {} not found. Message content : {}".format(
-                self.canal, self.message_content
-            )
-
-        else:
-            return "({}) {} : {}".format(canals[self.canal], self.name, self.message)
 
 
 if __name__ == "__main__":
